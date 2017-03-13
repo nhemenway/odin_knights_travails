f270ac350661dc7089047e2a7a091d664186ba67
Nicholas Hemenway <n@nhem.co>
2017-03-12 20:56:48 -0700

    refactored

--- kt.rb
+++ kt.rb
@@ -1,32 +1,3 @@
-class Board
-  attr_accessor :cells
-
-  def initialize
-    @cells = []
-    make_board
-  end
-
-  def make_board
-    (0...8).each do |row|
-      (0...8).each do |column|
-        @cells << [row, column]
-      end
-    end
-  end
-
-  def on_board?(ary)
-    (0...8).include?(ary[0]) && (0...8).include?(ary[1]) ? true : false
-  end
-
-  # for testing board
-  def display 
-    @cells.each_slice(8).to_a.each do |row|
-      print row
-      puts
-    end
-  end
-end
-
 class Move
   attr_reader :position, :parent
 
@@ -38,10 +9,7 @@ end
 
 
 class Knight
-  attr_accessor :position
-
-  def initialize(position = [0,0], board = Board.new())
-    @board = board
+  def initialize
   end
 
   def available_moves(ary)
@@ -51,13 +19,19 @@ class Knight
                  [r - 1, c + 2], [r - 1, c - 2],
                  [r + 2, c + 1], [r + 2, c - 1],
                  [r - 2, c + 1], [r - 2, c - 1]] 
-    all_moves.each { |move| legal_moves << move if @board.on_board?(move) }
+    all_moves.each { |move| legal_moves << move if on_board?(move) }
     legal_moves
   end
 
+  def on_board?(ary)
+    (0...8).include?(ary[0]) && (0...8).include?(ary[1]) ? true : false
+  end
+
   def knight_moves(start_cell, end_cell)
+    return p "not valid" unless on_board?(start_cell) && on_board?(end_cell)
+
     current_cell = Move.new(start_cell)
-    queue = [current_cell] 
+    queue = [current_cell]
 
     until current_cell.position == end_cell do
       current_cell = queue.shift
@@ -66,42 +40,26 @@ class Knight
 
     current_cell    
 
-    moves = []
+    moves = [start_cell]
     count = 0
-    until current_cell.nil?
+    until current_cell.parent.nil?
       moves << current_cell.position
+      count += 1
       current_cell = current_cell.parent
     end
 
-    moves.reverse!
+    show_moves(moves.reverse, count)
   end
 
-
- # def breadth_first_search(value)
- #    queue = [@root]
-
- #    until queue.empty?
- #      node = queue.shift
- #      return node if value == node.value
- #      queue << node.left unless node.left.nil?
- #      queue << node.right unless node.right.nil?
- #    end
- #    return nil
- #  end
-
-
-
+  def show_moves(moves, count)
+    puts "You made it in #{count} moves!"
+    p moves.each { |move| move}
+  end
 
 
 end
 
-# c =  Cell.new(1,1)
-# p c.to_a
-# b = Board.new
-# p b.on_board?([-2, 1])
-
 k = Knight.new()
-# p k.available_moves([1,0])
-
-p k.knight_moves([0,0],[1,1])
-
+k.knight_moves([0,0],[1,1])
+k.knight_moves([0,0],[0,0])
+k.knight_moves([0,0],[0,9])
